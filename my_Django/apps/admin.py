61b375a79dadb0352c784e17e7bc8ec9cad7d99f
Paul <53886153+zzq13-14@users.noreply.github.com>
2020-03-05 16:36:31 +0800

    Add files via upload

---
+++ my_Django/apps/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Article
+# Register your models here.
+# class ArticleAdimin(admin.ModelAdmin):
+#     list_display = ['title', 'content','brief_content', 'publist_date']
+#
+#
+# admin.site.register(Article, ArticleAdimin)
